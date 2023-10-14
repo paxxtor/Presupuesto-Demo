bf7f386c720ca6328d3340d71fce0c0eb164dee8
Paxtor <miguepaxtor@gmail.com>
2023-10-14 00:30:01 -0600

    ingresos y egresos con js finalizado

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const ingresos = [
-  new Ingreso("Salario", 2100.00),
+  new Ingreso("Salario", 2100.0),
   new Ingreso("Venta coche", 1500),
 ];
 
 const egresos = [
   new Egreso("Renta Departamento", 900),
-  new Egreso("ropa", 400),
+  new Egreso("Ropa", 400),
 ];
 
 let cargarApp = () => {
   cargarCabecero();
+  cargarIngresos();
+  cargarEgresos();
 };
 
 let totalIngresos = () => {
@@ -29,31 +31,78 @@ let totalEgresos = () => {
 };
 
 let cargarCabecero = () => {
-    let presupuesto = totalIngresos() - totalEgresos();
-    let porcentajeEgreso = totalEgresos() / totalIngresos();
-    if(presupuesto>0) document.getElementById("presupuesto").innerHTML = "+" + formatoMoneda(presupuesto);
-    else document.getElementById("presupuesto").innerHTML =  presupuesto;
-    document.getElementById("porcentaje").innerHTML = formatoPorcentaje(porcentajeEgreso);
-    document.getElementById("ingresos").innerHTML = formatoMoneda(totalIngresos());
-    document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+  let presupuesto = totalIngresos() - totalEgresos();
+  let porcentajeEgreso = totalEgresos() / totalIngresos();
+  if (presupuesto > 0)
+    document.getElementById("presupuesto").innerHTML =
+      "+" + formatoMoneda(presupuesto);
+  else document.getElementById("presupuesto").innerHTML = presupuesto;
+  document.getElementById("porcentaje").innerHTML =
+    formatoPorcentaje(porcentajeEgreso);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
 };
 
-const formatoMoneda = (valor) =>{
-    return valor.toLocaleString('en-US',{style:'currency', currency:'USD', minimumFractionDigits:2});
-}
+const formatoMoneda = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
 
-const formatoPorcentaje = (valor) =>{
-    return valor.toLocaleString('en-US',{style:'percent',minimumFractionDigits: 1});
-}
+const formatoPorcentaje = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 1,
+  });
+};
 
-const cargarIngresos = () =>{
-    let ingresosHTML = '';
-    for(let ingreso of ingresos){
-        ingresosHTML += crearIngresoHTML(ingreso);
-    }
-    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
-}
+const cargarIngresos = () => {
+  let ingresosHTML = "";
+  for (let ingreso of ingresos) {
+    ingresosHTML += crearIngresoHTML(ingreso);
+  }
+  document.getElementById("lista-ingresos").innerHTML = ingresosHTML;
+};
 
-const crearIngresoHTML = (ingreso) =>{
-    
-}
+const cargarEgresos = () => {
+  let egresosHTML = "";
+  for (let egreso of egresos) {
+    egresosHTML += crearEgresosHTML(egreso);
+  }
+  document.getElementById("lista-egresos").innerHTML = egresosHTML;
+};
+
+const crearIngresoHTML = (ingreso) => {
+  let ingresoHTML = `<div class="elemento limpiarEstilos">
+  <div class="elemento_descripcion">${ingreso.Descripcion}</div>
+  <div class="derecha limpiarEstilos">
+    <div class="elemento_valor">+${formatoMoneda(ingreso.Valor)}</div>
+    <div class="elemento_eliminar">
+      <button class="elemento_eliminar--btn">
+          <ion-icon name="close-circle-outline"></ion-icon>
+      </button>
+    </div>
+  </div>
+</div>`;
+  return ingresoHTML;
+};
+
+const crearEgresosHTML = (egreso) => {
+  let egresosHTML = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.Descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">-${egreso.Valor}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje(egreso.Valor / totalEgresos())}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+        <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>`;
+  return egresosHTML;
+};
