92c521f93c6e266d8e97381bddc0afc7e9ac6898
Paxtor <miguepaxtor@gmail.com>
2023-10-13 23:00:20 -0600

    Fin de cabecero

--- js/app.js
+++ js/app.js
@@ -1,8 +1,47 @@
 const ingresos = [
-    new Ingreso('Salario',2100.00),
-    new Ingreso('Venta coche', 1500)
+  new Ingreso("Salario", 2100.00),
+  new Ingreso("Venta coche", 1500),
 ];
 
 const egresos = [
-    new Egreso('')
+  new Egreso("Renta Departamento", 900),
+  new Egreso("ropa", 400),
 ];
+
+let cargarApp = () => {
+  cargarCabecero();
+};
+
+let totalIngresos = () => {
+  let totalIngreso = 0;
+  for (let ingreso of ingresos) {
+    totalIngreso += ingreso.Valor;
+  }
+  return totalIngreso;
+};
+
+let totalEgresos = () => {
+  let totalEgreso = 0;
+  for (let egreso of egresos) {
+    totalEgreso += egreso.Valor;
+  }
+  return totalEgreso;
+};
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
+    if(presupuesto>0) document.getElementById("presupuesto").innerHTML = "+" + formatoMoneda(presupuesto);
+    else document.getElementById("presupuesto").innerHTML =  presupuesto;
+    document.getElementById("porcentaje").innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById("ingresos").innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
+
+const formatoMoneda = (valor) =>{
+    return valor.toLocaleString('en-US',{style:'currency', currency:'USD', minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = (valor) =>{
+    return valor.toLocaleString('en-US',{style:'percent',minimumFractionDigits: 1});
+}
